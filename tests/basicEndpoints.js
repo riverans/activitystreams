30bec0263279af24190e58db0bc07b1b868acd6b
Farhan Syed <nearhan@gmail.com>
2014-01-02 14:54:33 -0500

    added remove and delete tests

--- tests/basicEndpoints.js
+++ tests/basicEndpoints.js
@@ -53,6 +53,7 @@ describe('Test Basic Api End Points', function() {
                 done();
             });
         });
+
         it('check resposne for specfic objet typed verbed by actor', function(done) {
             baseUrl.pathname += 'user/1/favorited/picture';
             var apiUrl = url.format(baseUrl);
@@ -62,6 +63,7 @@ describe('Test Basic Api End Points', function() {
 
             });
         });
+
         it('check response for specfic activity', function (done) {
             baseUrl.pathname += 'user/1/favorited/picture/1';
             var apiUrl = url.format(baseUrl);
@@ -71,7 +73,40 @@ describe('Test Basic Api End Points', function() {
             });
         });
 
+        it('check response for posting specific activity', function (done) {
+            baseUrl.pathname += 'activity';
+            var apiUrl = url.format(baseUrl);
+
+            reqOptions = {
+                url: apiUrl,
+                method: 'POST',
+                body: JSON.stringify({
+                    actor: {
+                        type: 'mmdb_user',
+                        mmdb_user_id: 1
+                    },
+                    object: {
+                        type: 'yourshot_photo',
+                        yourshot_photo_id: '1'
+                    },
+                    verb: {
+                        type: 'FAVORITED'
+                    }
+                })
+            }
+            request.post(reqOptions, function (err, response, body) {
+                assert.equal(response.statusCode, 200);
+                done();
+            });  
+        });
+
+        it("check response for deleting specfic activity", function(done) {
+            baseUrl.pathname += 'user/1/FAVORITED/picture/1'
+            var apiUrl = url.format(baseUrl);
+            request.del(apiUrl, function (err, response, body) {
+                asset.equal(response.statusCode, 200);
+                done();
+            });
+        });
     });
 });
-
-
