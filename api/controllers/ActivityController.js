e8f64442ab5ed39f2c237d1c02680789a6586010
Ben Fonarov <ben.fonarov@gmail.com>
2014-01-24 15:35:15 -0500

    trying to nail down how groc parses jsdoc

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -2,17 +2,17 @@
 *
 * @module ActivityController
 * 
-* @desc A set of functions defining the Activity Stream API.
+* @description A set of functions defining the Activity Stream API.
 *
-* Actions contain code telling Sails how to respond to a certain type of request.
-* (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+*  Actions contain code telling Sails how to respond to a certain type of request.
+*  (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
 *
-* You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
-* and/or override them with custom routes (`config/routes.js`)
+*  You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+*  and/or override them with custom routes (`config/routes.js`)
 *
-* NOTE: The code you write here supports both HTTP and Socket.io automatically.
+*  NOTE: The code you write here supports both HTTP and Socket.io automatically.
 *
-* @see http://sailsjs.org/#!documentation/controllers
+*  see http://sailsjs.org/#!documentation/controllers
 */
 
 module.exports = {
