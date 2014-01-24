7e3a3afc5bb216708d91b4c91935c31be78f23be
Ben Fonarov <ben.fonarov@gmail.com>
2014-01-24 15:12:11 -0500

    ActivityController module documentation

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -1,18 +1,18 @@
 /**
-* ActivityController
 *
-* @module      :: Controller
-* @description	:: A set of functions called `actions`.
+* @module ActivityController
+* 
+* A set of functions defining the Activity Stream API.
 *
-*                 Actions contain code telling Sails how to respond to a certain type of request.
-*                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+* Actions contain code telling Sails how to respond to a certain type of request.
+* (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
 *
-*                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
-*                 and/or override them with custom routes (`config/routes.js`)
+* You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+* and/or override them with custom routes (`config/routes.js`)
 *
-*                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+* NOTE: The code you write here supports both HTTP and Socket.io automatically.
 *
-* @docs        :: http://sailsjs.org/#!documentation/controllers
+* @docs :: http://sailsjs.org/#!documentation/controllers
 */
 
 module.exports = {
