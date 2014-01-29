bcbe3bed8b56f9692e17087775a425c36fd285b1
Ben Fonarov <ben.fonarov@gmail.com>
2014-01-29 13:43:49 -0500

    format api docs

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -4,13 +4,13 @@
 * @module      :: Controller
 * @description	:: A set of functions called `actions`.
 *
-*   Actions contain code telling Sails how to respond to a certain type of request.
-*   (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+*     Actions contain code telling Sails how to respond to a certain type of request.
+*     (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
 *
-*   You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
-*   and/or override them with custom routes (`config/routes.js`)
+*     You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+*     and/or override them with custom routes (`config/routes.js`)
 *
-*   NOTE: The code you write here supports both HTTP and Socket.io automatically.
+*     NOTE: The code you write here supports both HTTP and Socket.io automatically.
 *
 * @docs        :: http://sailsjion/controllerss.org/#!documentat
 */
