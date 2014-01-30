c463869549313f11a7e4c748adc10804e6e2e980
Ben Fonarov <ben.fonarov@gmail.com>
2014-01-30 16:49:36 -0500

    activity api update

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -85,6 +85,19 @@ module.exports = {
 					}
 				}
 
+				{
+					actor: {
+						id: "17",
+						data: {
+							created: 1388691303471,
+							appname_model_id: "1",
+							updated: 1388789935187,
+							appname_model_api: "http://reallycool.api.url/path/1/",
+							type: "appname_model"
+						}
+					}
+				}
+
 		+ Example
 
 				{
@@ -112,6 +125,65 @@ module.exports = {
 			}
 		);
 	},
+
+	/**
+	###### Actor [GET][/{appname_model}/{appname_model_id}]
+	An activity is always started by an Actor. This endpoint will return the node representing an actor of specific id.
+
+	All actors have the following data:
+
+	- id (this id is assigned by Neo4j and should not be used)
+
+	And within the data property:
+
+	- created: _Timestamp_
+	- updated (optional): _Timestamp_
+	- appname_model_id: _Integer/String_
+	- appname_model_api: _URL_
+	- type: {appname_model}
+
+	+ Parameters
+		+ appname_model (string) where appname is the name of the app, and model is the name of the model we are dealing with (eg. mmdb_user or ngm_article)
+		+ appname_model_id (integer) a number that matches an appropriate id from another applications model
+
+	+ Model (application/json)
+
+		JSON representation of an actor node
+
+		+ Headers
+
+			
+
+		+ Body
+
+				{
+					actor: {
+						id: "17",
+						data: {
+							created: 1388691303471,
+							appname_model_id: "1",
+							updated: 1388789935187,
+							appname_model_api: "http://reallycool.api.url/path/1/",
+							type: "appname_model"
+						}
+					}
+				}
+
+		+ Example
+
+				{
+					actor: {
+						id: "17",
+						data: {
+							created: 1388691303471,
+							mmdb_user_id: "1",
+							updated: 1388789935187,
+							mmdb_user_api: "https://mmdb.nationalgeographic.com/user/1",
+							type: "mmdb_user"
+						}
+					}
+				}
+	*/
 	getSpecificActor: function(req, res) {
 		var obj = {}, q, key;
 		key = req.param('actor') + '_id';
@@ -127,6 +199,103 @@ module.exports = {
 			}
 		);
 	},
+	/**
+	###### Actor [GET][/{appname_model}/{appname_model_id}/{verb}]
+	This endpoint will return activities in the db that match the actor and specific verb specified in the params.
+
+	Activity:
+
+	- id (this id is assigned by Neo4j and should not be used)
+
+	And within the data property:
+
+	- created: _Timestamp_
+	- updated (optional): _Timestamp_
+	- appname_model_id: _Integer/String_
+	- appname_model_api: _URL_
+	- type: {appname_model}
+
+	+ Parameters
+		+ appname_model (string) where appname is the name of the app, and model is the name of the model we are dealing with (eg. mmdb_user or ngm_article)
+		+ appname_model_id (integer) a number that matches an appropriate id from another applications model
+		+ verb (string) where verb is the name of a valid activity type attached to that user
+
+	+ Model (application/json)
+
+		JSON representation of an actor node
+
+		+ Headers
+
+			
+
+		+ Body
+
+				{
+					actor: {
+						id: "17",
+						data: {
+							created: 1388691303471,
+							mmdb_user_id: "1",
+							updated: 1388789935187,
+							mmdb_user_api: "https://mmdb.nationalgeographic.com/user/1/",
+							type: "mmdb_user"
+						}
+					},
+					verb: {
+						id: "50",
+						data: {
+							created: 1388777568757
+						},
+						type: "FAVORITED",
+						start: "17",
+						end: "18"
+					},
+					object: {
+						id: "18",
+						data: {
+							yourshot_photo_api: "http://yourshot.nationalgeographic.com/api/v1/photo/14055",
+							created: 1388692564273,
+							yourshot_photo_id: "14055",
+							updated: 1388777568757,
+							type: "yourshot_photo"
+						}
+					}
+				}
+
+		+ Example
+
+				{
+					actor: {
+						id: "17",
+						data: {
+							created: 1388691303471,
+							mmdb_user_id: "1",
+							updated: 1388789935187,
+							mmdb_user_api: "https://mmdb.nationalgeographic.com/user/1/",
+							type: "mmdb_user"
+						}
+					},
+					verb: {
+						id: "50",
+						data: {
+							created: 1388777568757
+						},
+						type: "FAVORITED",
+						start: "17",
+						end: "18"
+					},
+					object: {
+						id: "18",
+						data: {
+							yourshot_photo_api: "http://yourshot.nationalgeographic.com/api/v1/photo/14055",
+							created: 1388692564273,
+							yourshot_photo_id: "14055",
+							updated: 1388777568757,
+							type: "yourshot_photo"
+						}
+					}
+				}
+	*/
 	getAllObjectsVerbedByActor: function(req, res) {
 		var obj = {}, q, key;
 		key = req.param('actor') + '_id';
