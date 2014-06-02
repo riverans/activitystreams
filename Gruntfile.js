e58475de9dc918aa33b23b3a3c6abcbb4367f699
Ben Fonarov <ben.fonarov@gmail.com>
2014-06-02 16:37:40 -0400

    updated grunt file to do nothing on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -459,10 +459,10 @@ module.exports = function (grunt) {
   // Build the assets into a web accessible folder.
   // (handy for phone gap apps, chrome extensions, etc.)
   grunt.registerTask('build', [
-    'compileAssets',
-    'linkAssets',
-    'clean:build',
-    'copy:build'
+    // 'compileAssets',
+    // 'linkAssets',  -- commented out for now since we have nothing really to build
+    // 'clean:build',
+    // 'copy:build'
   ]);
 
   // When sails is lifted in production
