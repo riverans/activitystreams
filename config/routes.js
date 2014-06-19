7d868e2de5b18bc5218d34ce5c0315613b3cb45b
Farhan Syed <nearhan@gmail.com>
2014-06-19 15:04:18 -0400

    updated to 0.0.5

--- config/routes.js
+++ config/routes.js
@@ -30,16 +30,9 @@ module.exports.routes = {
 
   // By default, your root route (aka home page) points to a view
   // located at `views/home/index.ejs`
-  // 
+  //
   // (This would also work if you had a file at: `/views/home.ejs`)
 
-  '/loadtest': {
-    view: 'home/loadtest'
-  },
-
-  '/as-demo': {
-    view: 'home/as-demo'
-  },
 
   '/index': {
     view: 'home/index'
@@ -109,7 +102,7 @@ module.exports.routes = {
   // If you want to set up a route only for one in particular
   // (GET, POST, PUT, DELETE, etc.), just specify the verb before the path.
   // For example, if you have a `UserController` with a `signup` action,
-  // and somewhere else, you're serving a signup form looks like: 
+  // and somewhere else, you're serving a signup form looks like:
   //
   //		<form action="/signup">
   //			<input name="username" type="text"/>
@@ -138,7 +131,7 @@ module.exports.routes = {
 
 
 
-/** 
+/**
  * (3) Action blueprints
  * These routes can be disabled by setting (in `config/controllers.js`):
  * `module.exports.controllers.blueprints.actions = false`
@@ -204,4 +197,3 @@ module.exports.routes = {
  * Finally, if nothing else matched, the default 404 handler is triggered.
  * See `config/404.js` to adjust your app's 404 logic.
  */
- 
