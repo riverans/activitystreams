ad13422e50df0dbecef08a732051f7ee9b3d14a0
Lucas Serven <lserven@gmail.com>
2014-08-14 17:55:00 -0400

    Ensure fake neo4j is listening

--- tests/root_hooks.js
+++ tests/root_hooks.js
@@ -17,21 +17,21 @@ var sails = require('sails'),
 // Note that it is possible to run tests against the live database
 // by omitting the use of Nock.  Be sure your server is running
 // otherwise you will receive Uncaught Error: connect ECONNREFUSED
-var neo4j = express().use('/db/data/', function(req, res) {
-        res.send(200);
-    }),
-    neo4jServer = http.createServer(neo4j).listen(7474);
-
 before(function (done) {
     process.env.testMode = true; // enable mock responses from api/controllers/ActivityController.js
     process.env.testModeDebug = false; // cypher queries printed to console
     http.globalAgent.maxSockets = 100;
-    sails.lift({
-        port: 9365,
-        adapters: {
-            default: 'neo4j'
-        }
-    }, done);
+    var neo4j = express().use('/db/data/', function(req, res) {
+            res.send(200);
+        }),
+        neo4jServer = http.createServer(neo4j).listen(7474, function() {
+            sails.lift({
+                port: 9365,
+                adapters: {
+                    default: 'neo4j'
+                }
+            }, done);
+        });
 });
 
 beforeEach(function(done) {
