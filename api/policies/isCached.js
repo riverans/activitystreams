5a49e7533c8d67b0e673412c106e449b16adb396
Ben Fonarov <ben.fonarov@gmail.com>
2014-09-22 11:43:34 -0400

    hotfix for caching issue and websockets +ver

--- api/policies/isCached.js
+++ api/policies/isCached.js
@@ -6,7 +6,7 @@ module.exports = function(req, res, next) {
     Caching.read(req.url).then(
         function(reply) {
             reply = JSON.parse(reply);
-            if (req.get('if-none-match') && reply.etag && req.get('if-none-match') === reply.etag) {
+            if (!req.isSocket && req.get('if-none-match') && reply.etag && req.get('if-none-match') === reply.etag) {
                 return res.send(304);
             }
             return res.send(reply.data);
