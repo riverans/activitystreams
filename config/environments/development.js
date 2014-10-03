87f42a9222c530a188537db485132c9f490ab794
Ariel Rodriguez <ariel.delfor@globant.com>
2014-10-03 16:56:38 -0300

    Removing non public environment config.

---
+++ config/environments/development.js
@@ -0,0 +1,16 @@
+module.exports = {
+    port: 9365,
+    environment: 'development',
+
+    //auth policy configuration ==============================
+    authPolicy : {
+
+        //endpoint configuration ======================================
+        endpoint: {
+            host: '',
+            port: '',
+            path: '',
+            sessionCookie: '',
+        },
+    }
+}
