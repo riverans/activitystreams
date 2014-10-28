riverans/activitystreams

commit 4f5536dcfc124511987d2e4d9ffcd4005e99193d

fix caching responses from flattening data

api/services/Caching.js
@@ -80,8 +80,9 @@ module.exports = {
                 inverted: req.route.path.indexOf('/api/v1/object') === 0 ? true : false,
                 req: req,
                 custom: custom
-            },
-            members = this._generateMembers(options);
+            };
+            options.flat = FlattenData(options.data);
+            var members = this._generateMembers(options);
 
         return new Promise(function(resolve, reject) {
             /** Select keyspace with routes as primary keys. */
@@ -182,7 +183,8 @@ module.exports = {
             writeMembers = {},
             activity,
             custom = options.custom,
-            data = options.data,
+            // FYI: we only use flattened data to generate the members, but shouldn't store it as the response
+            data = options.data[0].items ? options.flat : options.data,
             depth = options.depth || 1,
             inverted = options.inverted || false,
             member,

api/controllers/ActorController.js
@@ -303,7 +303,6 @@ module.exports = {
                     // return res.json(err);
                     res.json(500, { error: 'INVALID REQUEST' });
                 }
-                results = FlattenData(results);
                 res.json(results);
                 Caching.write(req, results, 3);
             });
@@ -457,7 +456,6 @@ module.exports = {
                     // return res.json(err);
                     res.json(500, { error: 'INVALID REQUEST' });
                 }
-                results = FlattenData(results);
                 res.json(results);
                 Caching.write(req, results, 4);
             });

api/controllers/ObjectController.js
@@ -38,7 +38,6 @@ module.exports = {
                     // return res.json(err);
                     res.json(500, { error: 'INVALID REQUEST' });
                 }
-                results = FlattenData(results);
                 res.json(results);
                 Caching.write(req, results, 5);
             });
@@ -163,7 +162,6 @@ module.exports = {
                     // return res.json(err);
                     res.json(500, { error: 'INVALID REQUEST' });
                 }
-                results = FlattenData(results);
                 res.json(results);
                 Caching.write(req, results, 3);
             });

commit 53994643b39c24ed688be43e6352c8dbb7b8b77f

Correct errors introduced in 4f5536dcfc124511987d2e4d9ffcd4005e99193d

api/services/Caching.js
@@ -40,15 +40,15 @@ client.on('error', function (err) {
 module.exports = {
     /**
      * Read a response from the cache.
-     * @param {string} url The url to fetch.
-     * @return {object} A Promise object that resolves with the cached data.
+     * @param {string} [url] - The url to fetch.
+     * @returns {object} A Promise object that resolves with the cached data.
      */
     read: function(url) {
         if (sails.config.cacheActive === false) {
             return new Promise(function(resolve, reject) {
                 return reject(200);
             });
-        };
+        }
 
         /** Standardize all urls to have a trailing slash. */
         url = (url.substr(-1) !== '/') ? url + '/' : url;
@@ -62,22 +62,21 @@ module.exports = {
                 if (err) {
                     sails.log.error('Error from cache: ', err);
                     return reject(500);
-                };
+                }
                 if (reply) {
-                    sails.log.debug('Caching read.')
                     return resolve(reply);
-                };
+                }
                 return reject(404);
             });
         });
     },
     /**
      * Write a response to the cache.
-     * @param {object} req The request object.
-     * @param {object} data The response that should be saved in the cache.
-     * @param [integer] depth The depth of the members that should be used for the new key.
-     * @param [string] custom A custom string to use for cache-busting.
-     * @return {object} A Promise object that resolves with the written data.
+     * @param {object} req - The request object.
+     * @param {object} data - The response that should be saved in the cache.
+     * @param {integer} [depth] - The depth of the members that should be used for the new key.
+     * @param {string} [custom] - A custom string to use for cache-busting.
+     * @returns {object} A Promise object that resolves with the written data.
      * Depth = 1: actor_type/aid.VERB.object_type/aid
      * Depth = 2: actor_type/aid.VERB.object_type/ actor_type/.VERB.object_type/aid
      * Depth = 3: actor_type/aid.VERB. .VERB.object_type/aid
@@ -87,14 +86,10 @@ module.exports = {
     write: function(req, data, depth, custom) {
         if (sails.config.cacheActive === false) {
 
-            sails.log.debug('Write. Ignore cache.');
-
             return new Promise(function(resolve, reject) {
                 return resolve();
             });
         };
-
-        sails.log('Caching write.');
 
         var replacer = sails.express.app.get('json replacer'),
             spaces = sails.express.app.get('json spaces'),
@@ -115,7 +110,7 @@ module.exports = {
                  * If the data is empty, then we need to generate some data
                  * from the params only for member generation, not for caching,
                  */
-                data: data || this._generateDataFromReq(req),
+                data: this._data(req, data),
                 depth: depth,
                 /**
                  * If we are writing form the point of view of an object then
@@ -124,9 +119,8 @@ module.exports = {
                 inverted: req.route.path.indexOf('/api/v1/object') === 0 ? true : false,
                 req: req,
                 custom: custom
-            };
-            options.flat = FlattenData(options.data);
-            var members = this._generateMembers(options);
+            },
+            members = this._generateMembers(options);
 
         return new Promise(function(resolve, reject) {
             /** Select keyspace with routes as primary keys. */
@@ -145,9 +139,9 @@ module.exports = {
 
     /**
      * Bust a cached response and invalidate necessary fields.
-     * @param {object} req The request object.
-     * @param {object} activities The activities that should be busted.
-     * @return {object} A Promise that resolves when the bust is done.
+     * @param {object} req - The request object.
+     * @param {object} activities - The activities that should be busted.
+     * @returns {object} A Promise that resolves when the bust is done.
      */
     bust: function(req, data) {
         var bustMember,
@@ -156,9 +150,8 @@ module.exports = {
             noun,
             url;
 
-        data = data || this._generateDataFromReq(req);
+        data = this._data(req, data);
 
-        sails.log.debug('Caching Bust.');
         /** Select keyspace with members as primary keys. */
         client.select(2);
 
@@ -218,9 +211,9 @@ module.exports = {
 
     /**
      * Generate the list of members to use for writing and busting.
-     * @param {object} options A hash of options to generate members including:
+     * @param {object} options - A hash of options to generate members including:
      * data, depth, and inverted.
-     * @return {object} An object containing members suitable for both writing
+     * @returns {object} An object containing members suitable for both writing
      * to and busting the cache.
      */
     _generateMembers: function(options) {
@@ -228,8 +221,7 @@ module.exports = {
             writeMembers = {},
             activity,
             custom = options.custom,
-            // FYI: we only use flattened data to generate the members, but shouldn't store it as the response
-            data = options.data[0].items ? options.flat : options.data,
+            data = options.data,
             depth = options.depth || 1,
             inverted = options.inverted || false,
             member,
@@ -319,8 +311,8 @@ module.exports = {
      * Some requests do not return data, such as GET requests for non existant
      * data and DELETE requests, so we need to create a processable activity or
      * node from the request parameters.
-     * @param {object} req The request object.
-     * @return {array} An array of length 1 with the generated activity or
+     * @param {object} req - The request object.
+     * @returns {array} An array of length 1 with the generated activity or
      * node.
      */
     _generateDataFromReq: function(req) {
@@ -350,5 +342,55 @@ module.exports = {
         }
 
         return [data];
+    },
+
+    /**
+     * Not all the controller responses use the same data structure. Some
+     * responses must be flattened so that they resemble others and all the
+     * data can be fed to the same caching methods.
+     * @param {array} data The data to flatten.
+     * @returns {array} An array of activities.
+     */
+    _flattenData: function(data) {
+        var activities = [];
+        for (var i = 0; i < data.length; i++) {
+            for (var key in data[i].items) {
+                if (data[i].items.hasOwnProperty(key)) {
+                    activities.push(data[i].items[key]);
+                }
+            }
+        }
+        return activities;
+    },
+
+    /**
+     * The responses from the controllers are not always suitable for member
+     * generation. This method ensures the data exists and is formatted
+     * adequately.
+     * @param {object} req - The request object.
+     * @param {array} [data] - The data to analyze.
+     * @returns {array} An array of activities.
+     */
+    _data: function(req, data) {
+        /**
+         * If no data is supplied, respond with activities generated from the
+         * request
+         */
+        if (!data) return this._generateDataFromReq(req);
+
+        /**
+         * If the data contains `items` attributes, then flatten if the list is
+         * not empty, otherwise generate data from the request.
+         */
+        if (data[0].totalItems) {
+            if (data[0].totalItems > 0) return this._flattenData(data);
+            return this._generateDataFromReq(req);
+        }
+
+        /**
+         * If we've gotten this far, then the incoming data should be properly
+         * structured for processing.
+         */
+        return data;
     }
 };

api/services/FlattenData.js
@@ -1,21 +1,0 @@
-/*jslint node: true */
-'use strict';
-
-/**
- * Not all the controller responses use the same data structure. Some
- * responses must be flattened so that they resemble others and all the
- * data can be fed to the same caching methods.
- * @param {array} data The data to flatten.
- * @return {array} An array of activities.
- */
-module.exports = function(data) {
-    var activities = [];
-    for (var i = 0; i < data.length; i++) {
-        for (var key in data[i].items) {
-            if (data[i].items.hasOwnProperty(key)) {
-                activities.push(data[i].items[key]);
-            }
-        }
-    }
-    return activities;
-};