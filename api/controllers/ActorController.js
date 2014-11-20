bace73a2db666ed0d58bc3167ce46f9e06a4b499
Matias M. Montenegro <matias.montenegro@globant.com>
2014-11-20 18:09:28 -0300

    pagination in getAllActivitiesByActor

--- api/controllers/ActorController.js
+++ api/controllers/ActorController.js
@@ -444,7 +444,10 @@ module.exports = {
     */
 
     getAllActivitiesByActor: function(req, res) {
-        var obj = {}, q;
+        var obj = {}, q, limit, offset=0,
+            limit = ('limit' in req.query) ? Number(req.query.limit) : 50,
+            offset = ('offset' in req.query) ? Number(req.query.offset) : 1;
+
         q = [
             'MATCH (actor:' + req.param('actor') + ')-[verb]->(object)',
             'WHERE actor.aid="' + req.param('actor_id') + '"',
@@ -457,6 +460,22 @@ module.exports = {
                     // return res.json(err);
                     res.json(500, { error: 'INVALID REQUEST' });
                 }
+
+                var init = (offset-1) * limit,
+                    end = init + limit,
+                    reduced = {},
+                    key;
+
+                if ('items' in results[0]){
+                    for (var i = init; i < end; i++) {
+                        key = i.toString();
+                        if(key in results[0].items){
+                            reduced[key] = results[0].items[key]
+                        }
+                    };
+                    results[0].items = reduced;
+                }
+
                 res.json(results);
                 Caching.write(req, results, 4);
             });
