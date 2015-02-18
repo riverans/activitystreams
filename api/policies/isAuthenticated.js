d615cc434892ccfdc3439f3b025aa245f4d4c44b
dughetti <dughetti@zesmind.com>
2015-02-18 12:27:45 -0300

    Delete comment

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -1,13 +1,9 @@
-/** host: 'http://mmdb.dev.nationalgeographic.com',
-            port: 8000,
-            path: '/api/v1/session/',
-            sessionCookie: 'mmdbsessionid'
+/**
  * isAuthenticated
  *
  * @module      :: Policy
  * @description :: Implement authentication policy
  * @docs        :: http://sailsjs.org/#!documentation/policies
- *
  */
 var request = require('request');
 var https = require('https');
